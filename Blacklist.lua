caa6d8d57ee6735e2b3d95bda790f5ff586703a5
RyoFucker <140109256+RyoFucker@users.noreply.github.com>
2023-10-15 19:43:52 +0200

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -4,11 +4,11 @@ Blacklistusers = {
   "JLennonHD_Gamer",
   "DONOVAVN100",
   "mandird17",
-  "iiTr4xshXrSweaty",
   "firesperet55",
   "TheGodRisingAboveAll",
   "Nev3rH3ard87",
   "Oni_akumon",
   "condori147",
   "iiTr4xshXrSweaty",
+  "limberthgamer",
 }
