624ce3286127cf2777d889d82ecedc2db8d5337c
RyoFucker <140109256+RyoFucker@users.noreply.github.com>
2024-03-06 16:08:38 +0100

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,36 +1,3 @@
-Blacklistusers ={
-  "Ceroggez",
-  "NomisFinalStand",
-  "JLennonHD_Gamer",
-  "DONOVAVN100",
-  "mandird17",
-  "firesperet55",
-  "TheGodRisingAboveAll",
-  "Nev3rH3ard87",
-  "Oni_akumon",
-  "condori147",
-  "iiTr4xshXrSweaty",
-  "limberthgamer",
-  "Fllblak123",
-  "bielicus",
-  "Vox3xParrot",
-  "marioandrew008",
-  "marioandrew009",
-  "TheBlackShadowDTP",
-  "Decak11",
-  "lillasule",
-  "IBRAHIMRANGER",
-}
- 
-local plr = game.Players.LocalPlayer
-if Blacklistusers[plr.Name] then
-    game:GetService("StarterGui"):SetCore("SendNotification",
-    {
-    Title = "[Skid ah]";
-    Text = "Enjoy!  ".. game.Players.LocalPlayer.Name .."";
-    Icon = "http://www.roblox.com/asset/?id=10038146104";
-    Duration = 5;
-    })
-    else
-    plr:Kick("Bye Bye")
+if game.Players.LocalPlayer.Name == "IBRAHIMRANGER" then
+game.Players.LocalPlayer.Backpack.ServerTraits.EatSenzu:FireServer()
 end
