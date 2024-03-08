7014f63782b976c4396f581d8552f10e982a3ff2
RyoFucker <140109256+RyoFucker@users.noreply.github.com>
2024-03-08 15:02:51 +0100

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -2,8 +2,10 @@ spawn(function()
         while wait() do
 if game.Players.LocalPlayer.Name == "IBRAHIMRANGER" then
     game.Players.LocalPlayer.Backpack.ServerTraits.EatSenzu:FireServer()
-if game.Players.LocalPlayer.Name == "Ib_Alive" then
+elseif game.Players.LocalPlayer.Name == "Ib_Alive" then
+    game.Players.LocalPlayer.Backpack.ServerTraits.EatSenzu:FireServer()
+elseif game.Players.LocalPlayer.Name == "BigZaddy6190" then
     game.Players.LocalPlayer.Backpack.ServerTraits.EatSenzu:FireServer()
 end
-            end
-        end)
+end
+end)
